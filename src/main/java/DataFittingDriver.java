e27b13c6db454208bbd9cde5109781e749bd8eb5
Vachana Shetty <vachanagshetty@gmail.com>
2020-04-02 05:11:34 -0700

    Kmeans partial

--- src/main/java/DataFittingDriver.java
+++ src/main/java/DataFittingDriver.java
@@ -14,6 +14,7 @@ public class DataFittingDriver {
       dp.addData(Double.valueOf(sc.next()), Double.valueOf(sc.next()));
     }
     List<Point2D> value = dp.dataList();
+    dp.kmeans(5);
 //    for(int i=0;i<value.size();i++){
 //      System.out.println(value.get(i).x+ " : t : "+value.get(i).y);
 //    }
